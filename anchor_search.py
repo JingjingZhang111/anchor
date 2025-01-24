f2b30abbcee2b86b24f3b901f2704696a5645e07
JingjingZhang111 <13098695056@163.com>
2025-01-24 15:34:07 +0800

    Update and rename anchor111.py to anchor_search.py

--- anchor_search.py
+++ anchor_search.py
@@ -1,71 +1,56 @@
-from sys import exit
 from itertools import product
-from collections import Counter
-import gc
 import numpy as np
-from Bio import SeqIO
-import math, re
-from statistics import mean
-
+import re
 
 def rc_seq(seq):
     complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
     anchor_seq = ''.join(complement[base] for base in reversed(seq))
     return anchor_seq
 
-
 def re_seq(seq):
     return seq[::-1]
-
 
 def generate_kmer_dict(k):
     bases = 'ACGT'
     kmer_dict = {}
-    # 生成所有可能的k-mer
     for seq in product(bases, repeat=k):
         kmer = ''.join(seq)
         kmer_dict[kmer] = 0
     return kmer_dict
 
-def kmer1(a,c,l):
-    b={}
-    for k,v in a.items():
+def kmer1(a, c, l):
+    b = {}
+    for k, v in a.items():
         if k[:c] not in b.keys():
-            b[k[:c]]=v
+            b[k[:c]] = v
         else:
-            b[k[:c]]=b[k[:c]]+v
+            b[k[:c]] = b[k[:c]] + v
 
-    b={ke:list(np.sort(list(set(va)))) for ke, va in b.items()}
-    gaps={}
-    for ke,va in b.items():
-        
-        if len(va)>2:
-            
-            gap=[va[i+1]-va[i] for i in range(len(va)-1)]
-            #gap.append(l-va[-1]+va[0])
-            sd=sum(gap)
-            gap = np.sum([(-i / sd) * np.log(i / sd) for i in gap]) * (((l - va[-1]) / l * np.log((l - va[-1]) / l)))
-
+    b = {ke: list(np.sort(list(set(va)))) for ke, va in b.items()}
+    gaps = {}
+    for ke, va in b.items():
+        if len(va) > 2:
+            gap = [va[i + 1] - va[i] for i in range(len(va) - 1)]
+            sd = sum(gap)
+            gap = np.sum([(-i / sd) * np.log(i / sd) for i in gap])*(((l - va[-1]) / l * np.log((l - va[-1]) / l)))
         else:
-            gap=0
-        gaps[ke]=gap
-
+            gap = 0
+        gaps[ke] = gap
     return gaps
-
 
 def anchor_(k, epsilon, seqs):
     name = seqs[0]
     sequence = seqs[1]
     l = len(sequence)
     anchor = {}
-    for i in range(len(sequence) - k  + 1):
+    for i in range(len(sequence) - k + 1):
         kmer = sequence[i:i + k]
         if bool(re.search(r'[^A,C,G,T]', kmer)):
             continue
         rc_kmer = rc_seq(kmer)
         re_kmer = re_seq(kmer)
         for n in range(epsilon):
-            if min(i + k + n,i + k * 2 + n)>l:
+            if min(i + k + n, i + k * 2 + n) > l:
                 continue
             sub_seq = sequence[i + k + n:i + k * 2 + n]
             if sub_seq == rc_kmer:
@@ -79,14 +64,10 @@ def anchor_(k, epsilon, seqs):
             if key not in anchor:
                 anchor[key] = []
             anchor[key].append(i)
-    b=kmer1(anchor,k,l)
-
+    b = kmer1(anchor, k, l)
     base_dict = generate_kmer_dict(k)
     for key, value in b.items():
         if key in base_dict:
             base_dict[key] = value
     vector = [base_dict[key] for key in base_dict]
     return [name, vector]
-
-
-
